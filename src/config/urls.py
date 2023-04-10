f35a30f4dcb1c2fc774771d15939574d6cde73d2
orcakid <orcakid97@gmail.com>
2023-04-10 19:38:59 +0300

    simple-jwt

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from lor import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 router = routers.SimpleRouter()
 router.register('champs', views.PersViewSet, basename='champs')
@@ -27,5 +27,8 @@ router.register('champs', views.PersViewSet, basename='champs')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
